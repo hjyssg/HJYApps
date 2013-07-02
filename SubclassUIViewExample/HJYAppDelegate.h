5efb524fd480117205a861becd502b60d4d1af8a
juh42 <junyang_huang@junyang-huangs-Mac-Pro.local>
2013-07-02 14:20:17 -0400

    commit 1.0
    add files

---
+++ SubclassUIViewExample/HJYAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  HJYAppDelegate.h
+//  SubclassUIViewExample
+//
+//  Created by junyang_huang on 7/2/13.
+//  Copyright (c) 2013 hjyssg.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class HJYViewController;
+
+@interface HJYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) HJYViewController *viewController;
+
+@end
